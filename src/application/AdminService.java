653666f8b434ffc5d08a51de984311a48f167a91
IHE4 <153078249+IHE4@users.noreply.github.com>
2024-05-01 16:26:27 +0200

    Add files via upload

---
+++ src/application/AdminService.java
@@ -0,0 +1,20 @@
+package application;
+public class AdminService {
+    public static void addMovie(Film film) {
+        MovieService.addFilm(film);
+    }
+
+    public static void removeMovie(String code) {
+        // Implementation of movie removal logic
+        System.out.println("Movie with code " + code + " removed successfully.");
+    }
+
+    public static void displayAllUsers() {
+        // Assuming a method in UserService that can return all users
+        for (User user : UserServices.users) {
+            System.out.println("User: " + user.getEmail());
+        }
+    }
+
+    
+}
