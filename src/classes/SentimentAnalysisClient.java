eb6ec0fd86db0825e1a13a370b0762a2966f4dc7
Richy Razafindandy <75550241+Chaotix953@users.noreply.github.com>
2024-05-21 13:37:00 +0200

    Add files via upload

---
+++ src/classes/SentimentAnalysisClient.java
@@ -0,0 +1,44 @@
+package classes;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+public class SentimentAnalysisClient {
+    public static void main(String[] args) {
+        try {
+            String text = "I loved this film";
+            String jsonInputString = "{\"text\": \"" + text + "\"}";
+
+            URL url = new URL("http://localhost:5000/predict");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json; utf-8");
+            conn.setRequestProperty("Accept", "application/json");
+            conn.setDoOutput(true);
+
+            try(OutputStream os = conn.getOutputStream()) {
+                byte[] input = jsonInputString.getBytes(StandardCharsets.UTF_8);
+                os.write(input, 0, input.length);           
+            }
+
+            int code = conn.getResponseCode();
+            System.out.println("Response Code : " + code);
+
+            try (BufferedReader br = new BufferedReader(
+                new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8))) {
+                StringBuilder response = new StringBuilder();
+                String responseLine;
+                while ((responseLine = br.readLine()) != null) {
+                    response.append(responseLine.trim());
+                }
+                System.out.println(response.toString());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
